51a8b72584ef06a8494d906bb2e192e724ba5c76
Bar Ifrah <71547715+BarIfrah@users.noreply.github.com>
2022-05-20 19:44:12 +0300

    Create rockPaperScissors.py

---
+++ rockPaperScissors.py
@@ -0,0 +1,49 @@
+# Rock Paper Scissors
+# A code file structure:
+# A line that starts with "#"  is a comment line (it will not be interpreted)
+"""
+If you ant to comment multiple lines, start and finish with three (3) double quotes (")
+As you can see, this line is also a comment.
+
+"""
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Here you include all of your package imports (like randome and time packages wev'e discussed about) 
+# ----------------------------------------------------------------------------------------------------------------------
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Here you will write all of the functions (for later stages of the course
+# ----------------------------------------------------------------------------------------------------------------------
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Here you write code :)
+# ----------------------------------------------------------------------------------------------------------------------
+"""
+I'll give you the text inputs for this program, to make your lives a little easier.
+In addition, and to make it simple to you, the input from the user will be an integer:
+1 for ROCK
+2 for PAPER
+3 for SCISSORS
+A text input describing the operation is unacceptable and will cost you with points.
+
+A quick reminder of the rules:
+
+ROCK(1) vs SCISSORS(3)   --> ROCK(1) wins
+SCISSORS(3) vs PAPER(2)  --> SCISSORS(3) win
+PAPER(2) vs ROCK(1)      --> PAPER(2) wins
+
+DO NOT ADD EXTRA OPTIONS (No lizard, no Spock.)
+"""
+
+# print the instructions for the user to see:
+print("GAME RULES: \n"
+     ROCK(1) vs SCISSORS(3)   --> ROCK(1) wins
+SCISSORS(3) vs PAPER(2)  --> SCISSORS(3) win
+PAPER(2) vs ROCK(1)      --> PAPER(2) wins)
+
+
+
+
+
+
+
