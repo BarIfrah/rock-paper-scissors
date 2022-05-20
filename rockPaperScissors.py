6e563c39f9793a69b9b6fc933ae7d5cf0240639c
Bar <ibari02@gmail.com>
2022-05-20 19:49:57 +0300

    Game instructions updated

--- rockPaperScissors.py
+++ rockPaperScissors.py
@@ -10,6 +10,7 @@ As you can see, this line is also a comment.
 # ----------------------------------------------------------------------------------------------------------------------
 # Here you include all of your package imports (like randome and time packages wev'e discussed about) 
 # ----------------------------------------------------------------------------------------------------------------------
+import random
 
 # ----------------------------------------------------------------------------------------------------------------------
 # Here you will write all of the functions (for later stages of the course
@@ -37,13 +38,16 @@ DO NOT ADD EXTRA OPTIONS (No lizard, no Spock.)
 
 # print the instructions for the user to see:
 print("GAME RULES: \n"
-     ROCK(1) vs SCISSORS(3)   --> ROCK(1) wins
-SCISSORS(3) vs PAPER(2)  --> SCISSORS(3) win
-PAPER(2) vs ROCK(1)      --> PAPER(2) wins)
+      "ROCK(1) vs SCISSORS(3)   --> ROCK(1) wins\n"
+      "SCISSORS(3) vs PAPER(2)  --> SCISSORS(3) win\n"
+      "PAPER(2) vs ROCK(1)      --> PAPER(2) wins)\n")
 
-
-
-
-
-
-
+# The game will run in a WHILE loop.
+# The loop is infinite, and only the user has the power to stop it (when they say they don't want to play anymore)
+while True:
+    """
+      This is the game's heart. You'll need to think and use everything we've learned so far to make this game work.
+      Remember Python's rules ( the ':' after a statement, the indentation with loops and statements..)
+      
+      """
+    pass
